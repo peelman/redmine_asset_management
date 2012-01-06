6126070ace1055455da55bfb79c29cda9495cff1
Nick Peelman <nick@peelman.us>
2012-01-06 12:09:40 -0500

    More Patching Loading TS...

---
+++ lib/redmine_asset_management/application_controller_patch.rb
@@ -0,0 +1,14 @@
+module RedmineAssetManagement
+  
+  # Patches Redmine's ApplicationController to include a RAM Helper
+  module ApplicationControllerPatch
+    def self.included(base) # :nodoc:
+      # Same as typing in the class
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        helper RAMHelper
+      end
+    end
+  end
+  
+end
--- lib/redmine_asset_management/issue_patch.rb
+++ lib/redmine_asset_management/issue_patch.rb
@@ -8,15 +8,5 @@ module RedmineAssetManagement
       base.class_eval do 
     end
   end
-  
-  # Patches Redmine's ApplicationController to include a RAM Helper
-  module ApplicationControllerPatch
-    def self.included(base) # :nodoc:
-      # Same as typing in the class
-      base.class_eval do
-        unloadable # Send unloadable so it will not be unloaded in development
-        helper RAMHelper
-      end
-    end
-  end
+
 end
