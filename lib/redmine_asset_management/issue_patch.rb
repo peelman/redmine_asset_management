1d9808bc08c78253be05817345ab4c3cf24ebd67
Nick Peelman <nick@peelman.us>
2012-01-06 12:18:58 -0500

    Patch Tweaks...

--- lib/redmine_asset_management/issue_patch.rb
+++ lib/redmine_asset_management/issue_patch.rb
@@ -6,7 +6,11 @@ module RedmineAssetManagement
       # Same as typing in the class
       base.class_eval do 
         unloadable # Send unloadable so it will not be unloaded in development
-        has_and_belongs_to_many :assets, :class_name => "RAMAsset", :join_table => "ram_issue_has_assets", :foreign_key => :issue_id, :associate_foreign_key => :asset_id
+        has_and_belongs_to_many :assets, 
+                                :join_table => "ram_issue_has_assets", 
+                                :class_name => "RAMAsset", 
+                                :foreign_key => "issue_id", 
+                                :associate_foreign_key => "asset_id"
       end
     end
   end
