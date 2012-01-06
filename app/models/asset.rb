9cc97734e59617aeb19a1c6a675e5925e72fefd5
Nick Peelman <nick@peelman.us>
2012-01-06 01:29:05 -0500

    Adding Asset Notes for keeping a running notation chain for a given asset
    Adding Asset MACs for tracking hardware with MAC addresses (computers, routers, switches, projectors, etc)
    
    Relocating a few pieces of the migration...

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,14 +1,15 @@
 class RAMAsset < ActiveRecord::Base
-  
+  acts_as_tree
   set_table_name "ram_assets"
   
   unloadable
   has_and_belongs_to_many :issues, :join_table => "ram_issue_has_assets"
   has_and_belongs_to_many :licenses, :class_name => "RAMLicense", :join_table => "ram_asset_has_licenses"
   has_and_belongs_to_many :categories, :class_name => "RAMAssetCategory", :join_table => "ram_asset_has_categories"
+  has_many :asset_notes, :class_name => "RAMAssetNote", :foreign_key => :asset_id
   has_many :children, :class_name => "RAMAsset", :foreign_key => :parent_id
   has_one :parent_id, :class_name => "RAMAsset"
-  has_one :location, :class_name => "RAMLocation"
+  has_one :default_location, :class_name => "RAMLocation"
   has_one :owned_by, :class_name => "User"
   has_one :created_by, :class_name => "User"
   has_one :updated_by, :class_name => "User"
--- db/migrate/001_setup_ram.rb
+++ db/migrate/001_setup_ram.rb
@@ -6,11 +6,12 @@ class RAMSetup < ActiveRecord::Migration
       t.string :make, :limit => 100, :null => false
       t.string :model, :serial, :limit => 100, :null => false, :default => nil
       t.string :expected_life, :limit => 50
-      t.string :notes, :limit => 1000, :null => true, :default => nil
-      t.integer :parent_id
+      t.integer :parent_id, :owned_by, :created_by, :updated_by, :default_location
       t.datetime :purchase_date
       t.string :purchase_price, :limit => 15
+      t.string :purchase_order_number, :limit => 50
       t.string :purchase_notes, :limit => 1000
+      t.datetime :warranty_expiration, :null => true
       t.boolean :retired, :default => false
       t.datetime :retired_date, :null => true
       t.timestamps
@@ -20,9 +21,22 @@ class RAMSetup < ActiveRecord::Migration
       t.string "name", "description"
       t.timestamps
     end
+
+    create_table :ram_asset_notes, :force => true do |t|
+      t.string "summary", :limit => 200
+      t.string "body", :limit => 2500
+      t.integer :asset_id, :created_by, :updated_by
+      t.timestamps
+    end
+    
+    create_table :ram_asset_mac_addresses, :force => true do |t|
+      t.string :mac, :limit => 21, :null => false
+      t.string :type, :limit => 25, :null => false
+      t.timestamps
+    end
     
     create_table :ram_licenses, :force => true do |t|
-      t.string :name, :publisher, :limit => 100, :null => false
+      t.string :name, :limit => 100, :null => false
       t.string :version, :limit => 15
       t.integer :type, :owned_by, :created_by, :updated_by
       t.integer :count
@@ -31,11 +45,12 @@ class RAMSetup < ActiveRecord::Migration
       t.datetime :expiration_date
       t.datetime :renewal_date
       t.datetime :download_date
+      t.string :publisher_name
+      t.string :publisher_url
       t.string :publisher_contact_name
       t.string :publisher_contact_phone
       t.string :publisher_contact_email
       t.string :registration_notes, :limit => 1000
-      t.string :publisher_url
       t.timestamps
     end
     
@@ -84,6 +99,8 @@ class RAMSetup < ActiveRecord::Migration
   def self.down
     drop_table :ram_assets
     drop_table :ram_asset_categories
+    drop_table :ram_asset_notes
+    drop_table :ram_asset_mac_addresses
     drop_table :ram_licenses
     drop_table :ram_license_keys
     drop_table :ram_license_types
