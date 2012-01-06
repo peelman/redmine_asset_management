2ad9f030dd252587019b305ed89706d9040ee24a
Nick Peelman <nick@peelman.us>
2012-01-06 12:13:50 -0500

    Missing an end...

--- lib/redmine_asset_management/issue_patch.rb
+++ lib/redmine_asset_management/issue_patch.rb
@@ -1,5 +1,4 @@
 module RedmineAssetManagement
-  
   # Patches Redmine's Issues dynamically. Adds a relationship
   # Issue +has_many+ to Asset
   module IssuePatch
@@ -8,6 +7,7 @@ module RedmineAssetManagement
       base.class_eval do 
         unloadable # Send unloadable so it will not be unloaded in development
         has_and_belongs_to_many :assets, :class_name => "RAMAsset", :join_table => "ram_issue_has_assets", :foreign_key => :issue_id, :associate_foreign_key => :asset_id
+      end
     end
   end
 end
