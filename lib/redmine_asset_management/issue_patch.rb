14988c7dc81cc8738b9f3f6f1206acec05d1218b
Nick Peelman <nick@peelman.us>
2012-01-06 12:07:38 -0500

    Troubleshooting my patch loading...

--- lib/redmine_asset_management/issue_patch.rb
+++ lib/redmine_asset_management/issue_patch.rb
@@ -1,19 +1,15 @@
 module RedmineAssetManagement
   
   # Patches Redmine's Issues dynamically. Adds a relationship
-  # Issue +has_many+ to IssueDepartment
+  # Issue +has_many+ to Asset
   module IssuePatch
     def self.included(base) # :nodoc:
       # Same as typing in the class
-      base.class_eval do
-        unloadable # Send unloadable so it will not be unloaded in development
-        has_and_belongs_to_many :issues, :class_name => "RAMAsset", :join_table => "ram_issue_has_assets", :foreign_key => :issue_id, :associate_foreign_key => :asset_id
-        
-      end
-   
+      base.class_eval do 
     end
   end
   
+  # Patches Redmine's ApplicationController to include a RAM Helper
   module ApplicationControllerPatch
     def self.included(base) # :nodoc:
       # Same as typing in the class
