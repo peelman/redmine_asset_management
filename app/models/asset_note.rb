2992d09f56df036c0380d5680dfd6fad68ccca02
Nick Peelman <nick@peelman.us>
2012-01-12 19:42:04 -0500

    Model Glitch, adding images, small GUI changes.

--- app/models/asset_note.rb
+++ app/models/asset_note.rb
@@ -3,7 +3,7 @@ class AssetNote < ActiveRecord::Base
   set_table_name "ram_asset_notes"
   unloadable
   
-  belongs_to :assets, :class_name => "Asset"
+  belongs_to :asset, :class_name => "Asset"
   has_one :created_by, :class_name => "User"
   has_one :updated_by, :class_name => "User"
 
